e09b440d1452cb6218401ac7b4ae2567d233ff1f
cytopia <cytopia@everythingcli.org>
2016-01-31 21:02:01 +0100

    Fixed errors, so it parses homebrew travis-ci checks

--- mysqldump-secure.rb
+++ mysqldump-secure.rb
@@ -1,15 +1,16 @@
 class MysqldumpSecure < Formula
-  desc "Encrypted mysqldump with compression, logging, blacklisting and Nagios/Icinga monitoring integration"
+  desc "Encrypted mysqldump with compression and logging"
   homepage "https://github.com/cytopia/mysqldump-secure"
   url "https://github.com/cytopia/mysqldump-secure/archive/0.11.3.tar.gz"
-  version "0.11.3"
   sha256 "046eb5da52d17f0a22d24a53380d820ce50dcff21b55047549ac61366fbc16a1"
 
   def install
-	#ENV.deparallelize
     system "./configure", "--prefix=#{prefix}"
-	system "make"
+    system "make"
     system "make", "reinstall"
   end
 
+  test do
+    system bin/"mysqldump-secure 2>&1 | grep ''"
+  end
 end
