aed83b4c97d1de17b0252a7f59bebf9660ba0ef7
cytopia <cytopia@everythingcli.org>
2016-02-04 17:22:46 +0100

    mysqldump-secure v0.14

--- mysqldump-secure.rb
+++ mysqldump-secure.rb
@@ -1,8 +1,8 @@
 class MysqldumpSecure < Formula
   desc "Encrypted mysqldump with compression and logging"
   homepage "https://github.com/cytopia/mysqldump-secure"
-  url "https://github.com/cytopia/mysqldump-secure/archive/0.13.tar.gz"
-  sha256 "3354312f25469bbbc66fd241f120ca8e88721cad89c7fc206979ce749245dc57"
+  url "https://github.com/cytopia/mysqldump-secure/archive/0.14.tar.gz"
+  sha256 "be707799fdda96f03a24e4983feab73f0c6a5c9443efcd4f131ef27574f8a4b0"
   head "https://github.com/cytopia/mysqldump-secure.git"
 
   def install
