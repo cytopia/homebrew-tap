3071ad38eef5b57628067bd5b8cbfbfbad324179
cytopia <cytopia@everythingcli.org>
2016-09-12 13:48:29 +0200

    awesome-ci fix download url

--- awesome-ci.rb
+++ awesome-ci.rb
@@ -1,7 +1,7 @@
 class AwesomeCi < Formula
   desc "Tools for git, file and static source code analysis."
   homepage "https://github.com/cytopia/awesome-ci"
-  url "https://github.com/cytopia/awesome-ci/archive/0.3.tar.gz"
+  url "https://github.com/cytopia/awesome-ci/archive/0.4.tar.gz"
   sha256 "6d2d7149295d2704a6c38ff3263f62318da9f1456c4cf63280fac9e4e6c890f9"
   head "https://github.com/cytopia/awesome-ci.git"
 
