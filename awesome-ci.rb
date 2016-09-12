ca9ec1e6353652abc0236d72dc24a066c407f385
cytopia <cytopia@everythingcli.org>
2016-09-12 13:47:13 +0200

    awesome-ci v0.4

--- awesome-ci.rb
+++ awesome-ci.rb
@@ -2,7 +2,7 @@ class AwesomeCi < Formula
   desc "Tools for git, file and static source code analysis."
   homepage "https://github.com/cytopia/awesome-ci"
   url "https://github.com/cytopia/awesome-ci/archive/0.3.tar.gz"
-  sha256 "04894980abcbf1581879eed04773a21339e694b5f75c36693aa2a3c742afee45"
+  sha256 "6d2d7149295d2704a6c38ff3263f62318da9f1456c4cf63280fac9e4e6c890f9"
   head "https://github.com/cytopia/awesome-ci.git"
 
   def install
